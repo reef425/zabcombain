0d30459337253c79968fe15df6f136294bb51a5b
Mukhutdinov Reef <reef425@gmail.com>
2018-02-23 22:52:12 +0500

    add test_suit_1

--- tests/testzabmodule.py
+++ tests/testzabmodule.py
@@ -13,3 +13,7 @@ class TestMethod(unittest.TestCase):
         {'hostid': '135','main': '0','type': '3','ip': '127.0.0.1'},
         {'hostid': '245','main': '0','type': '4','ip': '127.0.0.1'}
         )
+
+    def test_suit_1(self):
+        print(self.TESTLIST1)
+        self.assertTrue(False)
