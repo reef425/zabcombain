4e1b65a1e2b98f480a11c037e038f06b451d0b58
Mukhutdinov Reef <reef425@gmail.com>
2018-02-23 22:51:14 +0500

    add TESTLIST1

---
+++ tests/testzabmodule.py
@@ -0,0 +1,15 @@
+import unittest
+
+class TestMethod(unittest.TestCase):
+    """docstring for TestMethod."""
+    def __init__(self):
+        ##############   add TESTLIST1 ###################
+        self.TESTLIST1 = ({'hostid': '3425','main': '1','type': '1','ip': '127.0.0.1'},
+        {'hostid': '342','main': '1','type': '2','ip': '127.0.0.1'},
+        {'hostid': '425','main': '1','type': '3','ip': '127.0.0.1'},
+        {'hostid': '442','main': '1','type': '4','ip': '127.0.0.1'},
+        {'hostid': '465','main': '0','type': '1','ip': '127.0.0.1'},
+        {'hostid': '405','main': '0','type': '2','ip': '127.0.0.1'},
+        {'hostid': '135','main': '0','type': '3','ip': '127.0.0.1'},
+        {'hostid': '245','main': '0','type': '4','ip': '127.0.0.1'}
+        )
