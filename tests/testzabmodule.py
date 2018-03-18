ae10402747e607d001718e9ffe92fed5a0707523
Mukhutdinov Reef <reef425@gmail.com>
2018-03-19 04:43:10 +0500

    add test function testFromRemoteServerScript and setupApi

--- tests/testzabmodule.py
+++ tests/testzabmodule.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock
 from zabcombain import zabmodule
 
 class TestMethod(unittest.TestCase):
@@ -6,7 +7,18 @@ class TestMethod(unittest.TestCase):
     # def __init__(self,args):
     #     super(TestMethod, self).__init__(args)
 
+    def setupApi(self):
+        self.api = MagicMock()
+        self.api.script = MagicMock()
+        self.scriptValue = 'result value'
+        self.api.script.execute = MagicMock(return_value={'value':self.scriptValue})
+
+
     def setUp(self):
+        self.setupApi()
+        ###############################
+        self.host = zabmodule.initHost()
+        ##################################################
         self.lhost = 'localhost'
         self.iphost = '127.0.0.1'
         ##############   add TESTLIST1 ###################
@@ -42,8 +54,18 @@ class TestMethod(unittest.TestCase):
     def testChangeInterfaceList(self):
         self.assertEqual(zabmodule.changeInterfaceList(self.TESTLIST1), [{'hostid': '3425','main': '1','type': '1','ip': '127.0.0.1'}])
 
+    @unittest.skip("skip ping from OS")
     def testPingFromOS(self):
         self.assertEqual(zabmodule.pingFromOS(None),b"ip is None")
         self.assertEqual(zabmodule.pingFromOS(""),b"ip is empty")
         l = len(zabmodule.pingFromOS(self.iphost).decode().splitlines())
         self.assertEqual(l,8)
+
+    def testFromRemoteServerScript(self):
+        res = zabmodule.runRemoteServerScript(None,self.host)
+        self.assertEqual(res,'api is None')
+        res = zabmodule.runRemoteServerScript(self.api,None)
+        self.assertEqual(res,'host is None')
+        res = zabmodule.runRemoteServerScript(self.api,self.host)
+        self.assertEqual(self.host.get("pingresult"),[self.scriptValue.encode()])
+        self.assertEqual(res,'ok')
